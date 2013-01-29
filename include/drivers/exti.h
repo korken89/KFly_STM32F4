f8198ada257f4d007ec76caddd4e6f74240d1ca8
Emil Fresk <emil.fresk@gmail.com>
2013-01-29 18:11:33 +0100

    Added external interrupts initializers

---
+++ include/drivers/exti.h
@@ -0,0 +1,25 @@
+#ifndef __EXTI_H
+#define __EXTI_H
+
+/* Standard includes */
+#include "stm32f4xx.h"
+
+/* System includes */
+
+/* Scheduler includes. */
+
+/* KFly includes */
+
+/* Includes */
+
+/* Defines */
+
+/* Global variable defines */
+
+/* Typedefs */
+
+/* Global function defines */
+void EXTI_SensorInit(void);
+void EXTI_RCInputInit(void);
+
+#endif
---
+++ source/drivers/exti.c
@@ -0,0 +1,18 @@
+/* *
+ *
+ * External Interrupt driver.
+ * Used to check sensor data availability and RC input channels.
+ *
+ * */
+
+#include "exti.h"
+
+void EXTI_SensorInit(void)
+{
+
+}
+
+void EXTI_RCInputInit(void)
+{
+
+}
