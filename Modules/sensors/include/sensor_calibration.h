fa02d6b259acb1bd03d222435c7fc6a4fba88832
korken89 <ltu.korken@gmail.com>
2014-02-04 10:44:52 +0100

    Started Sensor Calibration structure

---
+++ Modules/sensors/include/sensor_calibration.h
@@ -0,0 +1,29 @@
+#ifndef __SENSOR_CALIBRATION_H
+#define __SENSOR_CALIBRATION_H
+
+/* Standard includes */
+
+/* System includes */
+
+/* Scheduler includes */
+
+/* KFly includes */
+
+/* Includes */
+
+/* Defines */
+
+/* Typedefs */
+typedef struct
+{
+	float accelerometer_bias[3];
+	float accelerometer_gain[3];
+	float magnetometer_bias[3];
+	float magnetometer_gain[3];
+} Sensor_Calibration_Type;
+
+/* Global variable defines */
+
+/* Global function defines */
+
+#endif
---
+++ Modules/sensors/source/sensor_calibration.c
@@ -0,0 +1,19 @@
+/* *
+ *
+ *
+ * */
+
+/* Includes */
+#include "sensor_calibration.h"
+
+/* Private Defines */
+
+/* Private Typedefs */
+
+/* Global variable defines */
+
+/* I2C Interrupt handlers */
+
+/* Private function defines */
+
+/* Conversion union for sensor data */
