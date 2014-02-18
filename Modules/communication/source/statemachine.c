64ad1d8ea4142e240c8e476f0ed6063d712e3227
korken89 <ltu.korken@gmail.com>
2014-02-18 17:36:28 +0100

    Added some parsers

--- Modules/communication/source/statemachine.c
+++ Modules/communication/source/statemachine.c
@@ -57,132 +57,132 @@
 #include "statemachine.h"
 
 const static Parser_Type parsers[127] = {
-	NULL,		/* 0: 	Cmd_None 						*/
-	NULL,		/* 1: 	Cmd_ACK 						*/
-	NULL,		/* 2: 	Cmd_Ping 						*/
-	NULL,		/* 3:	Cmd_DebugMessage				*/
-	NULL,		/* 4:	Cmd_GetRunningMode 				*/
-	NULL,		/* 5 */
-	NULL,		/* 6 */
-	NULL,		/* 7 */
-	NULL,		/* 8 */
-	NULL,		/* 9 */
-	NULL,		/* 10:	Cmd_PrepareWriteFirmware 		*/
-	NULL,		/* 11: 	Cmd_WriteFirmwarePackage 		*/
-	NULL,		/* 12:	Cmd_WriteLastFirmwarePackage	*/
-	NULL,		/* 13:	Cmd_ReadFirmwarePackage			*/
-	NULL,		/* 14:	Cmd_ReadLastFirmwarePackage		*/
-	NULL,		/* 15:	Cmd_NextPackage					*/
-	NULL,		/* 16:	Cmd_ExitBootloader				*/
-	NULL,		/* 17:	Cmd_GetBootloaderVersion		*/
-	NULL,		/* 18:	Cmd_GetFirmwareVersion			*/
-	NULL,		/* 19:	Cmd_SaveToFlash					*/
-	NULL,		/* 20 */
-	NULL,		/* 21 */
-	NULL,		/* 22 */
-	NULL,		/* 23 */
-	NULL,		/* 24 */
-	NULL,		/* 25 */
-	NULL,		/* 26 */
-	NULL,		/* 27 */
-	NULL,		/* 28 */
-	NULL,		/* 29 */
-	NULL,		/* 30:	Cmd_GetRateControllerData		*/
-	NULL,		/* 31:	Cmd_SetRateControllerData		*/
-	NULL,		/* 32:	Cmd_GetAttitudeControllerData	*/
-	NULL,		/* 33:	Cmd_SetAttitudeControllerData	*/
-	NULL,		/* 34:	Cmd_GetVelocityControllerData	*/
-	NULL,		/* 36:	Cmd_SetVelocityControllerData	*/
-	NULL,		/* 37:	Cmd_GetPositionControllerData	*/
-	NULL,		/* 38:	Cmd_SetPositionControllerData	*/
-	NULL,		/* 39:	Cmd_GetChannelMix				*/
-	NULL,		/* 40:	Cmd_SetChannelMix				*/
-	NULL,		/* 41:	Cmd_GetRCCalibration			*/
-	NULL,		/* 42:	Cmd_SetRCCalibration			*/
-	NULL,		/* 43:	Cmd_GetRCValues					*/
-	NULL,		/* 44:	Cmd_GetSensorData				*/
-	NULL,		/* 45 */
-	NULL,		/* 46 */
-	NULL,		/* 47 */
-	NULL,		/* 48 */
-	NULL,		/* 49 */
-	NULL,		/* 50 */
-	NULL,		/* 51 */
-	NULL,		/* 52 */
-	NULL,		/* 53 */
-	NULL,		/* 54 */
-	NULL,		/* 55 */
-	NULL,		/* 56 */
-	NULL,		/* 57 */
-	NULL,		/* 58 */
-	NULL,		/* 59 */
-	NULL,		/* 60 */
-	NULL,		/* 61 */
-	NULL,		/* 62 */
-	NULL,		/* 63 */
-	NULL,		/* 64 */
-	NULL,		/* 65 */
-	NULL,		/* 66 */
-	NULL,		/* 67 */
-	NULL,		/* 68 */
-	NULL,		/* 69 */
-	NULL,		/* 70 */
-	NULL,		/* 71 */
-	NULL,		/* 72 */
-	NULL,		/* 73 */
-	NULL,		/* 74 */
-	NULL,		/* 75 */
-	NULL,		/* 76 */
-	NULL,		/* 77 */
-	NULL,		/* 78 */
-	NULL,		/* 79 */
-	NULL,		/* 80 */
-	NULL,		/* 81 */
-	NULL,		/* 82 */
-	NULL,		/* 83 */
-	NULL,		/* 84 */
-	NULL,		/* 85 */
-	NULL,		/* 86 */
-	NULL,		/* 87 */
-	NULL,		/* 88 */
-	NULL,		/* 89 */
-	NULL,		/* 90 */
-	NULL,		/* 91 */
-	NULL,		/* 92 */
-	NULL,		/* 93 */
-	NULL,		/* 94 */
-	NULL,		/* 95 */
-	NULL,		/* 96 */
-	NULL,		/* 97 */
-	NULL,		/* 98 */
-	NULL,		/* 99 */
-	NULL,		/* 100 */
-	NULL,		/* 101 */
-	NULL,		/* 102 */
-	NULL,		/* 103 */
-	NULL,		/* 104 */
-	NULL,		/* 105 */
-	NULL,		/* 106 */
-	NULL,		/* 107 */
-	NULL,		/* 108 */
-	NULL,		/* 109 */
-	NULL,		/* 110 */
-	NULL,		/* 111 */
-	NULL,		/* 112 */
-	NULL,		/* 113 */
-	NULL,		/* 114 */
-	NULL,		/* 115 */
-	NULL,		/* 116 */
-	NULL,		/* 117 */
-	NULL,		/* 118 */
-	NULL,		/* 119 */
-	NULL,		/* 120 */
-	NULL,		/* 121 */
-	NULL,		/* 122 */
-	NULL,		/* 123 */
-	NULL,		/* 124 */
-	NULL,		/* 125 */
-	NULL		/* 126 */
+	NULL,						/* 0: 	Cmd_None 						*/
+	NULL,						/* 1: 	Cmd_ACK 						*/
+	vPing,						/* 2: 	Cmd_Ping 						*/
+	vGetRunningMode,			/* 3:	Cmd_DebugMessage				*/
+	NULL,						/* 4:	Cmd_GetRunningMode 				*/
+	NULL,						/* 5 */
+	NULL,						/* 6 */
+	NULL,						/* 7 */
+	NULL,						/* 8 */
+	NULL,						/* 9 */
+	NULL,						/* 10:	Cmd_PrepareWriteFirmware 		*/
+	NULL,						/* 11: 	Cmd_WriteFirmwarePackage 		*/
+	NULL,						/* 12:	Cmd_WriteLastFirmwarePackage	*/
+	NULL,						/* 13:	Cmd_ReadFirmwarePackage			*/
+	NULL,						/* 14:	Cmd_ReadLastFirmwarePackage		*/
+	NULL,						/* 15:	Cmd_NextPackage					*/
+	NULL,						/* 16:	Cmd_ExitBootloader				*/
+	vGetBootloaderVersion,		/* 17:	Cmd_GetBootloaderVersion		*/
+	vGetFirmwareVersion,		/* 18:	Cmd_GetFirmwareVersion			*/
+	NULL,						/* 19:	Cmd_SaveToFlash					*/
+	NULL,						/* 20 */
+	NULL,						/* 21 */
+	NULL,						/* 22 */
+	NULL,						/* 23 */
+	NULL,						/* 24 */
+	NULL,						/* 25 */
+	NULL,						/* 26 */
+	NULL,						/* 27 */
+	NULL,						/* 28 */
+	NULL,						/* 29 */
+	vGetRateControllerData,		/* 30:	Cmd_GetRateControllerData		*/
+	NULL,						/* 31:	Cmd_SetRateControllerData		*/
+	NULL,						/* 32:	Cmd_GetAttitudeControllerData	*/
+	NULL,						/* 33:	Cmd_SetAttitudeControllerData	*/
+	NULL,						/* 34:	Cmd_GetVelocityControllerData	*/
+	NULL,						/* 36:	Cmd_SetVelocityControllerData	*/
+	NULL,						/* 37:	Cmd_GetPositionControllerData	*/
+	NULL,						/* 38:	Cmd_SetPositionControllerData	*/
+	NULL,						/* 39:	Cmd_GetChannelMix				*/
+	NULL,						/* 40:	Cmd_SetChannelMix				*/
+	NULL,						/* 41:	Cmd_GetRCCalibration			*/
+	NULL,						/* 42:	Cmd_SetRCCalibration			*/
+	NULL,						/* 43:	Cmd_GetRCValues					*/
+	NULL,						/* 44:	Cmd_GetSensorData				*/
+	NULL,						/* 45 */
+	NULL,						/* 46 */
+	NULL,						/* 47 */
+	NULL,						/* 48 */
+	NULL,						/* 49 */
+	NULL,						/* 50 */
+	NULL,						/* 51 */
+	NULL,						/* 52 */
+	NULL,						/* 53 */
+	NULL,						/* 54 */
+	NULL,						/* 55 */
+	NULL,						/* 56 */
+	NULL,						/* 57 */
+	NULL,						/* 58 */
+	NULL,						/* 59 */
+	NULL,						/* 60 */
+	NULL,						/* 61 */
+	NULL,						/* 62 */
+	NULL,						/* 63 */
+	NULL,						/* 64 */
+	NULL,						/* 65 */
+	NULL,						/* 66 */
+	NULL,						/* 67 */
+	NULL,						/* 68 */
+	NULL,						/* 69 */
+	NULL,						/* 70 */
+	NULL,						/* 71 */
+	NULL,						/* 72 */
+	NULL,						/* 73 */
+	NULL,						/* 74 */
+	NULL,						/* 75 */
+	NULL,						/* 76 */
+	NULL,						/* 77 */
+	NULL,						/* 78 */
+	NULL,						/* 79 */
+	NULL,						/* 80 */
+	NULL,						/* 81 */
+	NULL,						/* 82 */
+	NULL,						/* 83 */
+	NULL,						/* 84 */
+	NULL,						/* 85 */
+	NULL,						/* 86 */
+	NULL,						/* 87 */
+	NULL,						/* 88 */
+	NULL,						/* 89 */
+	NULL,						/* 90 */
+	NULL,						/* 91 */
+	NULL,						/* 92 */
+	NULL,						/* 93 */
+	NULL,						/* 94 */
+	NULL,						/* 95 */
+	NULL,						/* 96 */
+	NULL,						/* 97 */
+	NULL,						/* 98 */
+	NULL,						/* 99 */
+	NULL,						/* 100 */
+	NULL,						/* 101 */
+	NULL,						/* 102 */
+	NULL,						/* 103 */
+	NULL,						/* 104 */
+	NULL,						/* 105 */
+	NULL,						/* 106 */
+	NULL,						/* 107 */
+	NULL,						/* 108 */
+	NULL,						/* 109 */
+	NULL,						/* 110 */
+	NULL,						/* 111 */
+	NULL,						/* 112 */
+	NULL,						/* 113 */
+	NULL,						/* 114 */
+	NULL,						/* 115 */
+	NULL,						/* 116 */
+	NULL,						/* 117 */
+	NULL,						/* 118 */
+	NULL,						/* 119 */
+	NULL,						/* 120 */
+	NULL,						/* 121 */
+	NULL,						/* 122 */
+	NULL,						/* 123 */
+	NULL,						/* 124 */
+	NULL,						/* 125 */
+	NULL						/* 126 */
 };
 
 /* *
