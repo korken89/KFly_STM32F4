8c652f288079ffc35573b1ffe7dabf2a9b887546
korken89 <ltu.korken@gmail.com>
2014-03-30 13:05:01 +0200

    Changed SPI frequency to APB2 / 16 = 5.25 MHz

--- Modules/hardware_drivers/source/spi.c
+++ Modules/hardware_drivers/source/spi.c
@@ -67,7 +67,7 @@ void SPI1Init(void)
   SPI_InitStructure.SPI_CPOL                = SPI_CPOL_High;
   SPI_InitStructure.SPI_CPHA                = SPI_CPHA_2Edge;
   SPI_InitStructure.SPI_NSS                 = SPI_NSS_Soft;
-  SPI_InitStructure.SPI_BaudRatePrescaler   = SPI_BaudRatePrescaler_4;
+  SPI_InitStructure.SPI_BaudRatePrescaler   = SPI_BaudRatePrescaler_16;
   SPI_InitStructure.SPI_FirstBit            = SPI_FirstBit_MSB;
   SPI_InitStructure.SPI_CRCPolynomial       = 7;
   SPI_Init(SPI1, &SPI_InitStructure); 
