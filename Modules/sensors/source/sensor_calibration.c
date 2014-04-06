80cc378eb43e80af790aad5d7fc58e1d59285030
korken89 <ltu.korken@gmail.com>
2014-04-06 14:39:56 +0200

    Updated the sensor calibration...

--- Modules/sensors/source/sensor_calibration.c
+++ Modules/sensors/source/sensor_calibration.c
@@ -23,15 +23,15 @@ void vInitSensorCalibration(Sensor_Calibration_Type *cal)
 	/* Set gains and biases */
 	cal->accelerometer_bias.x = 17.141f;
 	cal->accelerometer_bias.y = -29.604f;
-	cal->accelerometer_bias.z = 148.441f;
+	cal->accelerometer_bias.z = 57.441f;
 
-	cal->accelerometer_gain.x = 2048.294f;
-	cal->accelerometer_gain.y = 2047.527f;
-	cal->accelerometer_gain.z = 2077.573f;
+	cal->accelerometer_gain.x = 2044.294f;
+	cal->accelerometer_gain.y = 2046.527f;
+	cal->accelerometer_gain.z = 2079.573f;
 
-	cal->magnetometer_bias.x = 107.963f;
-	cal->magnetometer_bias.y = 42.558f;
-	cal->magnetometer_bias.z = -57.040f;
+	cal->magnetometer_bias.x = 97.963f;
+	cal->magnetometer_bias.y = 30.558f;
+	cal->magnetometer_bias.z = 57.040f;
 
 	cal->magnetometer_gain.x = 683.869f;
 	cal->magnetometer_gain.y = 648.546f;
